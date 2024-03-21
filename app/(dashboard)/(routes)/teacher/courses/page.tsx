ce2c2cecec0de02393dea5f3196d76a61d51ffba
Aman Khansili <amankhansilee@gmail.com>
2024-03-21 23:25:27 +0530

    Teacher portion

--- app/(dashboard)/(routes)/teacher/courses/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/page.tsx
@@ -1,5 +1,13 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
 const CoursesPage = () => {
-  return <div>Course page!</div>;
+  return (
+    <div className="p-4">
+      <Link href="/teacher/create">
+        <Button>New Course</Button>
+      </Link>
+    </div>
+  );
 };
 export default CoursesPage;
- 
